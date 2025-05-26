dfcdf238f8a16276c07f03fdab34f38a2bdce0e1
AnzoBenjamin <anzobnjmn@gmail.com>
2025-05-26 17:04:44 +0300

    minor changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APP_DOMAIN: 'https://vpoll.com.my',
-  API_DOMAIN: 'https://vpoll.com.my',
-  API_URL: 'https://vpoll.com.my/api',
+  APP_DOMAIN: 'http://localhost:8001/',
+  API_DOMAIN: 'http://localhost:8001/',
+  API_URL: 'http://localhost:8001//api',
 };
